a7fec18fddce30d5a91d80b7f23492ddfbefe060
l123-cpu <lucasng978@gmail.com>
2026-09-06 08:18:16 +0000

    Sep 6, 2026, 8:18 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,29 +1,2 @@
-//draw shape
-function setup(){
-createCanvas(600,400);
-background(0); //0 is black 255 is white
-}
-function draw(){
-fill(0);
-stroke(255,0,0);//change outline color
-strokeWeight(10);
-//triangle 6 parameters
-triangle(260,240,300,160,340,240);
-//circle 3 parameter x,y,d
-circle(150,200,80);
-//square
-rect(410,160,80,80);
-}
 
-//draw circle with forloop
-function setup(){
-createCanvas(600,400);
-background(220);
-}
-function draw(){
-for( let i =0; i<10; i++){
-fill(0 +i*50);
-circle(30*i+20,30 +i*20,20);
-}
 
-}
