a28bbddc8cda4e5b0ce57525d752791019719083
l123-cpu <lucasng978@gmail.com>
2026-09-06 08:13:03 +0000

    Sep 6, 2026, 8:13 AM

--- testquestion1.js
+++ testquestion1.js
@@ -1,11 +1,11 @@
-//draw shape
+
 function setup(){
 createCanvas(600,400);
-background(0); //0 is black 255 is white
+background(0); 
 }
 function draw(){
 fill(0);
-stroke(255,0,0);//change outline color
+stroke(255,0,0);
 strokeWeight(10);
 
 triangle(260,240,300,160,340,240);
